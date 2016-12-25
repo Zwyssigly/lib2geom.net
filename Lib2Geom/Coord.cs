296472ac7cf9846ed0f386132566beb3a37ea295
Stefan Zwyssig <add.stef@hotmail.com>
2016-12-25 18:16:47 +0100

    init

---
+++ Lib2Geom/Coord.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib2Geom
+{
+    public struct Coord 
+    {
+        public float Value { get; set; }
+    }
+}
---
+++ Lib2Geom/D2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lib2Geom
+{
+    public class D2<T>
+    {
+        private T[] _f = new T[2];
+
+        public T this[int i] => _f[i];
+
+        public D2(T a, T b)
+        {
+            _f[0] = a;
+            _f[1] = b;
+        }
+    }
+}
